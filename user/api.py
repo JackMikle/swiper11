0bf8ae715ab254d23e4cb86313550395d9ec77ca
jackMikle <clw206@qq.com>
2019-04-14 19:52:58 +0800

    1,add http render json 2,add keys and errors 3,add yzx sms 4,add user login and auto create user

---
+++ user/api.py
@@ -0,0 +1,62 @@
+import json
+
+from django.core.cache import cache
+from django.http import HttpResponse
+
+from lib.sms import send_sms
+from lib.http import render_json
+
+from common import errors, keys
+from user.models import User
+
+
+def submit_phone(request):
+    """获取短信验证码"""
+    if not request.method == "POST":
+        return render_json('request method error', errors.REQUEST_ERROR)
+
+    phone = request.POST.get('phone')
+    result,msg = send_sms(phone)
+
+    # return JsonResponse({'status':'ok','msg':msg})
+    data = {'status':'ok','msg':msg}
+    result = json.dumps(data)
+    return HttpResponse(result)
+
+
+def submit_vcode(request):
+    """通过验证码登录注册"""
+
+    #判断是否是post请求
+    if not request.method == "POST":
+        return render_json('request method error', errors.REQUEST_ERROR)
+
+    phone = request.POST.get('phone')
+    # 取到发到手机的验证码
+    vcode = request.POST.get('vcode')
+    # 取到缓存中的验证码
+    cache_vcode = cache.get(keys.VCODE_KEY % phone)
+
+    #对比验证码是否一致
+    if vcode == cache_vcode:
+        user, _ = User.objects.get_or_create(phonenum=phone, nickname=phone)
+        request.session['uid'] = user.id
+        return render_json(user.to_string())
+    else:
+        return render_json('verify code error',errors.VCODE_ERROR)
+
+def get_profile(request):
+    """获取个人资料"""
+    pass
+
+
+def set_profile(request):
+    """修改个人资料"""
+    pass
+
+
+def upload_avatar(request):
+    """头像上传"""
+    pass
+
+
